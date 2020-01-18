f2c4410fe2a0b5cf2232422917bb3d5863d9d9ef
raberin <rmvaberin@gmail.com>
2020-01-17 16:17:49 -0800

    Added repl parser to show options on terminal. Can quit the game.

--- src/adv.py
+++ src/adv.py
@@ -1,6 +1,8 @@
 from room import Room
 from player import Player
 
+spacer = '\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n'
+
 # Declare all the rooms
 
 room = {
@@ -37,10 +39,13 @@ room['treasure'].s_to = room['narrow']
 #
 # Main
 #
+print(spacer)
 print('Welcome to the Intro to Python II - Game')
+
+# Make a new player object that is currently in the 'outside' room.
 player_name = input('Please input your character name: ')
 new_player = Player(player_name, room['outside'])
-# Make a new player object that is currently in the 'outside' room.
+print(spacer)
 
 
 # Write a loop that:
@@ -53,3 +58,23 @@ new_player = Player(player_name, room['outside'])
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+isRunning = True
+while isRunning:
+    print(f"Your current location: {new_player.current_room.name}\n")
+    for current_room in room:
+        if new_player.current_room.name == room[current_room].name:
+            print(room[current_room].description)
+    print(
+        f"Where would you like to go? \n [n] North    [s] South   [e] East    [w] West    [q] Quit")
+    choice = input('Please input your choice here: ')
+    spacer = '\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n'
+
+    while choice not in ['n', 's', 'e', 'w', 'q']:
+        print(
+            "Invalid option. Please, choose: \n[n] North    [s] South   [e] East    [w] West    [q] Quit ")
+        choice = input('Please input your choice here: ')
+        spacer = '\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n'
+
+    if choice == 'q':
+        print('You are now quitting the game...\n Goodbye!')
+        isRunning = False
